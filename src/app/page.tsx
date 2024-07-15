cd3a9a55698cc4d5bd5db82c46bd9377f264be1d
Daryl <darylmicah12@gmail.com>
2024-07-15 17:47:00 +0530

    added cloudfare url to backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,9 @@ export default function Home() {
     const fetchData = async () => {
       if (!input) return setSearchResults(undefined);
 
-      const res = await fetch(`/api/search?q=${input}`);
+      const res = await fetch(
+        `https://fastapi.darylmicah12.workers.dev/api/search?q=${input}`
+      );
       const data = (await res.json()) as {
         results: string[];
         duration: number;
